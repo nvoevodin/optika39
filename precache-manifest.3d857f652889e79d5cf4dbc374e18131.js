d36894f60746432f8fa6827155b94922a25bc757
nvoevodin <46409820+nvoevodin@users.noreply.github.com>
2020-06-29 15:18:49 -0400

    Add files via upload

---
+++ precache-manifest.3d857f652889e79d5cf4dbc374e18131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811e3384aa855429f41a2ec56dba1a67",
+    "url": "/optika39/index.html"
+  },
+  {
+    "revision": "476c3027f8548dbf07e7",
+    "url": "/optika39/static/css/main.05716109.chunk.css"
+  },
+  {
+    "revision": "fa8aef005ad1313b4a7f",
+    "url": "/optika39/static/js/2.d8e85b6a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/optika39/static/js/2.d8e85b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "476c3027f8548dbf07e7",
+    "url": "/optika39/static/js/main.1ad1bcc8.chunk.js"
+  },
+  {
+    "revision": "088a0d735826cb9d4619",
+    "url": "/optika39/static/js/runtime-main.e1e1e26e.js"
+  }
+]);
